1788a1e6d264f770cc8b4d5488d20cbc4d7e628f
Schlomoh <post@moritzbecker.de>
2022-07-30 19:22:01 +0200

    add obj serialization

--- src/file_loading/mod.rs
+++ src/file_loading/mod.rs
@@ -1,19 +1,96 @@
 pub mod obj {
     use std::fs;
     use std::path::Path;
-    pub struct ObjFile {
+    pub struct File {
         pub file_name: String,
         pub file_path: String,
     }
 
-    pub trait LoadingMethods {
+    pub trait IOMethods {
         fn load_raw_file(&self) -> String;
     }
 
-    impl LoadingMethods for ObjFile {
+    impl IOMethods for File {
         fn load_raw_file(&self) -> String {
             let source = Path::new(&self.file_path).join(&self.file_name);
             return fs::read_to_string(source).expect("Failed loading file.");
+        }
+    }
+
+    pub struct GeometryData {
+        pub vertices: Vec<f32>,
+        pub normals: Vec<f32>,
+        pub texcoords: Vec<f32>,
+        pub faces: Vec<Vec<u32>>,
+    }
+
+    pub struct RawObjData {
+        pub raw_data: String,
+    }
+
+    pub trait ObjSerialization {
+        fn new(raw_data: String) -> Self;
+        fn parse_lines(&self) -> Vec<String>;
+        fn parse_geometry(&self) -> GeometryData;
+    }
+
+    impl ObjSerialization for RawObjData {
+        fn new(raw_data: String) -> Self {
+            return RawObjData { raw_data: raw_data };
+        }
+        fn parse_lines(&self) -> Vec<String> {
+            return self.raw_data.lines().map(|x| x.to_string()).collect();
+        }
+
+        fn parse_geometry(&self) -> GeometryData {
+            let lines = self.parse_lines();
+            let mut vertices = Vec::new();
+            let mut normals = Vec::new();
+            let mut texcoords = Vec::new();
+            let mut faces = Vec::new();
+
+            fn add_coord(vertices: &mut Vec<f32>, coords: &Vec<&str>) {
+                for coord in coords {
+                    let float = coord.replace(",", ".").parse::<f32>();
+                    match float {
+                        Ok(c) => vertices.push(c),
+                        Err(_) => eprintln!("Failed to convert coord"),
+
+                    }
+
+                }
+            }
+            fn add_faces(faces: &mut Vec<Vec<u32>>, parts: &Vec<&str>) {
+                let face = parts[1].split("/").collect::<Vec<&str>>();
+                let mut face_data = Vec::new();
+                for i in 1..face.len() {
+                    face_data.push(face[i].parse::<u32>().unwrap());
+                }
+                faces.push(face_data);
+            }
+
+            for line in lines {
+                let parts = line.split(" ").collect::<Vec<&str>>();
+                match parts[0] {
+                    "v" => {
+                        add_coord(&mut vertices, &parts[1..].to_vec());
+                    }
+                    "vn" => {
+                        add_coord(&mut normals, &parts[1..].to_vec());
+                    }
+                    "vt" => {
+                        add_coord(&mut texcoords, &parts[1..].to_vec());
+                    }
+                    "f" => add_faces(&mut faces, &parts),
+                    _ => {}
+                }
+            }
+            return GeometryData {
+                vertices,
+                normals,
+                texcoords,
+                faces,
+            };
         }
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
 mod file_loading;
-use file_loading::obj::{LoadingMethods, ObjFile};
+use file_loading::obj::{IOMethods, ObjSerialization, File, RawObjData};
 
 const BASE_INP_DIR: &str = "/Users/schlomoh/Documents/GitHub/rust_remeshing/_inp";
-const BASE_FILENAME: &str = "test";
+const BASE_FILENAME: &str = "test.obj";
 
 fn main() {
-    let file = ObjFile {
+    let file = File {
         file_path: BASE_INP_DIR.to_string(),
         file_name: BASE_FILENAME.to_string(),
     };
+
     let raw_data = file.load_raw_file();
-    for (i, c) in raw_data.lines().enumerate() {
-        println!("line: {}, text: {}", i, c)
-    }
+    println!("{}", raw_data);
+    let geometry = RawObjData::new(raw_data).parse_geometry();
+    println!("{:?}", geometry.vertices)
 }
